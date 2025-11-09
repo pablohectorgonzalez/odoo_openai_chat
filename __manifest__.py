77fff7775daa164d7a47b0da059b2831f675ecd5
pablohectorgai <pablohectorgonzalezjr@gmail.com>
2025-11-08 18:00:21 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,17 @@
 {
     'name': 'OpenAI Chat for Discuss',
-    'version': '17.0.1.0.9',
+    'version': '17.0.1.1.0',
     'summary': 'Conecta OpenAI al chat de Discuss mediante /ai o chat 1:1 con un bot',
     'author': 'MetaProject',
     'license': 'LGPL-3',
     'category': 'Productivity/Discuss',
-    'depends': ['mail'],
+    'depends': ["base", "mail", "discuss"],
     'data': [
         'views/res_config_settings_views.xml',
     ],
+    "external_dependencies": {
+        "python": ["requests", "openai"]  # 'openai-agents-python' opcional
+    },
     'assets': {},
     'application': False,
     'installable': True,
